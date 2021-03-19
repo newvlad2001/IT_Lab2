e497cc25fb936e6dd1f7b68a8c19026baf5502d4
newvlad2001 <newvlad2001@gmail.com>
2021-03-19 03:37:12 +0300

    Fixing work with bits

---
+++ IT_Lab2_Encryptor/BitArrayExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace IT_Lab2_Encryptor
+{
+    public static class BitArrayExtension
+    {
+        public static byte[] ToByteArray(this BitArray bits) 
+        {
+            /*int numBytes = bits.Count / 8;
+            if (bits.Count % 8 != 0) numBytes++;
+
+            byte[] bytes = new byte[numBytes];
+            int byteIndex = 0, bitIndex = 0;
+
+            for (int i = 0; i < bits.Count; i++) 
+            {
+                if (bits[i])
+                    bytes[byteIndex] |= (byte)(1 << (7 - bitIndex));
+
+                bitIndex++;
+                if (bitIndex == 8) {
+                    bitIndex = 0;
+                    byteIndex++;
+                }
+            }*/
+
+            byte[] bytes = new byte[(int) Math.Ceiling(bits.Count / 8.0)];
+            bits.CopyTo(bytes, 0);
+            return bytes;
+        }
+        
+        public static string ToBitString(this BitArray bits)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = bits.Count - 1; i > 0; i--)
+            {
+                char c = bits[i] ? '1' : '0';
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ IT_Lab2_Encryptor/Tester.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace IT_Lab2_Encryptor
+{
+    public class Tester
+    {
+        public static void Main(string[] args)
+        {
+            string name = "vladik";
+            byte[] b = {1};
+            BitArray a = new BitArray(b);
+            Console.WriteLine(a.ToBitString());
+            BitArray key = new BitArray(new bool[]
+            {
+                true, false, true, true, true, false, true, false,
+                true, false, true, false, true, false, true, false,
+                true, true, true, false, true, false, true, false,
+                true, false, true, false, true, false, true, false, true
+            });
+            Encryptor encryptor = new Encryptor();
+            BitArray data = new BitArray(System.Text.Encoding.Unicode.GetBytes(name));
+            Console.WriteLine("Input data: {0}", data.ToBitString());
+            BitArray result = encryptor.Encrypt(key, data);
+            Console.WriteLine("Cipher: {0}", result.ToBitString());
+            Console.WriteLine(System.Text.Encoding.Unicode.GetString(result.ToByteArray()));
+            result = encryptor.Encrypt(key, result);
+            Console.WriteLine("Plain: {0}", result.ToBitString());
+            Console.WriteLine(System.Text.Encoding.Unicode.GetString(result.ToByteArray()));
+        }
+    }
+}
